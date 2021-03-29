25811b85318abc34c27dc2c92517f560a32d0c17
UrbyX9 <klinc.urban97@gmail.com>
2021-03-29 09:49:50 +0200

    Add connect to dabase script

---
+++ src/inc/dbh.inc.php
@@ -0,0 +1,15 @@
+<?php
+    function pdo_connect_mysql(){
+        $dbHost = 'localhost';
+        $dbUser = 'root';
+        $dbPass = '';
+        $dbName = 'otiosum';
+
+        try{
+            return new PDO('mysql:host=' . $dbHost . ';dbname=' . $dbName . ';charset=utf8', $dbUser, $dbPass);
+        } catch (PDOException $exception) {
+            // If there is an error with the connection, stop the script and display the error.
+            die ('Failed to connect to database!'. $exception->getMessage());
+        }
+    }
+?>
