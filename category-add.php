7b6794e24ae1805a0622b3a96348a119cc73a7f9
UrbyX9 <klinc.urban97@gmail.com>
2021-06-22 14:44:18 +0200

    Change poisition ob button in category-add form, Add exception and roll back to the insert script in case of a failure on any insert,

--- category-add.php
+++ category-add.php
@@ -5,12 +5,11 @@
 ?>
 
 <div class="container">
-    <form method="post" enctype="multipart/form-data" action="./src/inc/category-insert.inc.php" >
-        <button id="submit-btn" type="submit" name="submit" class="btn btn-primary">Shrani</button>
-        <!-- <button class="add_form_field">Dodaj polje</button> -->
-
-
-
+    <!-- add button -->
+    <div class="d-flex justify-content-end m-2">
+        <button class="add_form_field btn btn-success float-right"><i class="fa fa-plus"></i></button>
+    </div>
+    <form id="ins-cat" method="post" enctype="multipart/form-data" action="./src/inc/category-insert.inc.php" >
 
         <div class="form-row">
             <div class="form-group col-md-5">
@@ -20,18 +19,18 @@
             <div class="form-group col-md-5">            
                 <label class="col-form-label" for="parent_category">Nad kategorija</label>
                 <select class="form-control" id="parent_category" name="parent_category[]">
-                    <option selected value="0"> ---N/A--- </option>
+                    <option selected value=""> ---N/A--- </option>
                     <?=categoryTree()?>
                 </select>
             </div>
         </div>
 
     </form>
-    <!-- button to add additional  -->
-    <div class="form-group col-md-1">
-        <button class="add_form_field btn btn-success"><i class="fa fa-plus"></i></button>
-    </div>
 
+    <!-- submit form button -->
+    <div class="d-flex justify-content-end m-2">
+        <button form="ins-cat" id="submit-btn" type="submit" name="submit" class="btn btn-primary float-right">Shrani</button>
+    </div>
 </div>
 
 <?php
