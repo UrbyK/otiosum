47c92d2ce1a2e6ced8f775c99be16c40e611d88b
UrbyX9 <klinc.urban97@gmail.com>
2021-07-13 22:35:33 +0200

    Add errror warning

--- src/inc/dbh.inc.php
+++ src/inc/dbh.inc.php
@@ -6,7 +6,7 @@
         $dbName = 'otiosum';
 
         try{
-            return new PDO('mysql:host=' . $dbHost . ';dbname=' . $dbName . ';charset=utf8', $dbUser, $dbPass);
+            return new PDO('mysql:host=' . $dbHost . ';dbname=' . $dbName . ';charset=utf8', $dbUser, $dbPass, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
         } catch (PDOException $exception) {
             // If there is an error with the connection, stop the script and display the error.
             die ('Failed to connect to database!'. $exception->getMessage());
