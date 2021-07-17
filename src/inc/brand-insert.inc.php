8e038a521ae45dfe21e970b9cbabe67bf5655eec
UrbyX9 <klinc.urban97@gmail.com>
2021-07-17 20:32:41 +0200

    Add error/exception checking

--- src/inc/brand-insert.inc.php
+++ src/inc/brand-insert.inc.php
@@ -1,7 +1,6 @@
 <?php
 
     require_once './session.php';
-    include_once './dbh.inc.php';
     include_once './image-upload.inc.php';
 
     if (!isLogin()) {
@@ -16,74 +15,60 @@
         header("Location: ../../brand-add");
         exit();
     } else {
-        $brand = $_POST['brand'];
+        // include db connection
+        include_once './dbh.inc.php';
+        $pdo = pdo_connect_mysql();
+
+        include_once './xss_cleaner.inc.php';
+        $brand = xss_cleaner($_POST['brand']);
         $img = $_FILES['img'];
+        print_r($img);
 
-        // careful at writing upload_image query, keep in mind that relative path is set in the
-        // the function and not here, keep an exstra '?' to execute 'data'
-        $query = "INSERT INTO brand (title, image) VALUES (?,?)";
-        $data = [$brand];
+        // check if brand value is given and not empty
+        if (isset($brand) && !empty($brand)) {
+            // check if image is given and not empty
+            if (isset($img) && !empty($img['name'])) {
+                // check if brand is unique
+                $stmt = $pdo->prepare("SELECT * FROM brand WHERE title LIKE ?");
+                $stmt->execute([$brand]);
+                if ($stmt->rowCount() == 0) {
+                    // careful at writing upload_image query, keep in mind that relative path is set in the
+                    // the function and not here, keep an exstra '?' to execute 'data'
+                    $query = "INSERT INTO brand (title, image) VALUES (?,?)";
+                    $stmt = $pdo->prepare($query);
+                    $uploadStatus = upload_image($brand, $img);
+                    
+                    // check if image upload is not error
+                    if (explode("=", $uploadStatus)[0] != "error") {
+                        // check if execute was successful
+                        if($stmt->execute([$brand, $uploadStatus])) {
+                            header("Location: ../../brand-add?status=success");
+                            exit();
+                        } else {
+                            header("Location: ../../brand-add?error=err");
+                            exit();
+                        }
 
-        $stat = upload_image($query,$data, $brand, $img);
+                    } else {
+                    header("Location: ../../brand-add?$uploadStatus");
+                    exit();
+                    }
+                    
+                } else {
+                    header("Location: ../../brand-add?error=brand-exists");
+                    exit();                   
+                }
+
+            } else {
+                header("Location: ../../brand-add?error=image-empty");
+                exit();
+            }
+
+        } else {
+            header("Location: ../../brand-add?error=brand-empty");
+            exit();
+        }
         
-        header ("Location: ../../brand-add?$stat");
-        exit();
-    //     $root = "../../uploads";
-
-    //     $year = date("Y");
-    //     $month = date("m");
-    //     $time = date("H").".".date("i").".".date("s");
-    //     // check if directory exists
-    //     if(!file_exists("$root/$year/$month")){
-    //         // create directory in case it doesn't exist
-    //         mkdir("$root/$year/$month", 0777, true);
-    //     }
-
-    //     // determine save location
-    //     $target_dir = "$root/$year/$month/";
-    //     $relative_dir = "./uploads/$year/$month/";
-    //     // get file extention (.txt, .jpg, .png, .php, .html, ...)
-    //     $imageFileType = strtolower(pathinfo($img['name'], PATHINFO_EXTENSION));
-    //     // rename saved image/file
-    //     $change_name = "$brand-$year-$month";
-    //     // save location
-    //     $target_file = $target_dir . $change_name.".".$imageFileType;
-    //     $relative_file = $relative_dir . $change_name.".".$imageFileType;
-
-    //     $uploadOK = 1;
-    //     // check if selected file is image
-    //     $check = getimagesize($_FILES["img"]["tmp_name"]);
-    //     if ($check == false) {
-    //         $uploadOK = 0;
-    //         header("Location: ../../brand-add?error=file-type");
-    //         exit();
-    //     }
-    //     // check for valid image extensions
-    //     if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "webp") {
-    //         $uploadOk = 0;
-    //         header("Location: ../../brand-add?error=img-type");
-    //         exit();
-    //     }
-    //     // check if image size is less then 1MB
-    //     if ($_FILES['img']['size'] > 1048576){
-    //         $UploadOK = 0;
-    //         header("Location: ../../brand-add?error=size");
-    //         exit();
-    //     }
-    //     // check if upload conditions are OK
-    //     if ($uploadOK == 1) {
-    //         if (move_uploaded_file($_FILES['img']['tmp_name'], $target_file)) {
-    //             $pdo = pdo_connect_mysql();
-    //             $query = "INSERT INTO brand (title, image) VALUES (?,?)";
-    //             $stmt = $pdo->prepare($query);
-    //             $stmt->execute([$brand, $relative_file]);
-    //             header("Location: ../../brand-add?status=ok");
-    //             exit();
-    //         }
-    //     }
-    //     header("Location: ../../brand-add?error=err");
-    //     exit();
-    // }
     }
 
 ?>
