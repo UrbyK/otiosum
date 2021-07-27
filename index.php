9ebf88c55e978e6e08f8d805d75813aa50e6cdf0
UrbyX9 <klinc.urban97@gmail.com>
2021-07-27 22:33:43 +0200

    Defaulted file0

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <?php
     include_once './header.php';
+?>
 
-    $token = md5("beseda");
-    echo "<p>$token</p>";
-
-
-    for ($i=0; $i <1000 ; $i++) { 
-        $token = bin2hex(random_bytes(30));
-        echo "<p>$token</p>";
-    }
-
+<?php
     include_once './footer.php';
-
 ?>
