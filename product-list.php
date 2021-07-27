447e8d9ca8c6a230bb25b0f33d6af64612597eea
UrbyX9 <klinc.urban97@gmail.com>
2021-07-27 22:28:08 +0200

    Add pagination (20 items per page)

--- product-list.php
+++ product-list.php
@@ -1,45 +1,64 @@
-
 <?php 
     include_once './header.php';
+
+    if (!isLogin() && !isAdmin() && !isMod()) {
+        exit("<script>window.location.href='index'</script>");
+    }
+    
+    $pdo =pdo_connect_mysql();
+    // number of list items per page
+    $num_of_product_per_page = 20;
+    // URL this will appear as index.php?page=products&p=1, index.php?page=products&p=2 ...
+    $current_page = isset($_GET['p']) && is_numeric($_GET['p']) ? (int)$_GET['p'] : 1;
+    // select products by latest
+    $stmt = $pdo->prepare('SELECT * FROM product ORDER BY id DESC LIMIT ?,?');
+    // bindValue allows us to use integer in SQL, need it for LIMIT
+    $stmt->bindValue(1, ($current_page - 1) * $num_of_product_per_page, PDO::PARAM_INT);
+    $stmt->bindValue(2, $num_of_product_per_page, PDO::PARAM_INT);
+    $stmt->execute();
+    // fetch and return products as ARRAY
+    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // total amount of products
+    $total_products = $pdo->query('SELECT * FROM product')->rowCount();
+
+    #$starts_from = ($current_page-1)*$num_of_product_per_page;
+
+    $total_pages = ceil($total_products/$num_of_product_per_page);
+
 ?>
 
+
 <div class="container">
-        <div class="row mt-3">
+    <div class="card my-3">
+        <div class="card-header text-center">
+            <h2 class="subtitle">List izdelkov</h2>
+        </div>
+        <div class="row mx-2">
             <div class="col-md-12">     
                 <div class="mt-3">
                     <ul class="list list-inline">
-                    <?php foreach (products() as $product): ?>
+                        <?php foreach ($products as $product): ?>
                         <li class="d-flex justify-content-between border rounded mt-2">
-                            <div class="d-flex flex-row align-items-center">
-                                <div class="ml-2">
-                                    <h5 class="mb-0"><?=$product['title']?></h5>
-                                    <h6 class="ml-3">SKU: <?=$product['sku']?></h6>
-                                </div>
-                            </div>
-                            <div class="d-flex flex-row align-items-center">
-                                <?php foreach(productImages($product['id']) as $image): ?>
-                                    <img src="<?=$image['image']?>" alt="<?=$image['caption']?>" class="rounded-circle" style="height:75px;">
-                                <?php endforeach;?>
-                                <div class="ml-2">
-                                    <h6 class="mb-0"><?=$product['title']?></h6>
-                                    <div class="d-flex flex-row mt-1 text-black-50 date-time">
-                                        <div>
-                                            <i class="fa fa-calendar-o"></i><span class="ml-2">22 May 2020 11:30 PM</span>
-                                        </div>
-                                        <div class="ml-3">
-                                            <i class="fa fa-clock-o"></i><span class="ml-2">6h</span>
-                                        </div>
+                            <div class="d-flex flex-row align-items-center col-5">
+                                <div class="ml-2 mt-2">
+                                    <h5 class="mb-0"><b><?=$product['title']?></b></h5>
+                                    <div class="d-flex ">
+                                        <h6 class="ml-3">SKU: <?=$product['sku']?></h6>
+                                        <h6 class="ml-3">CENA: <?=$product['price']?> €</h6>
                                     </div>
                                 </div>
                             </div>
-                            <div class="d-flex flex-row align-items-center">
+                            <div class="d-flex flex-row align-items-center col-5">
+                                <?php foreach(productImages($product['id']) as $image): ?>
+                                    <img src="<?=$image['image']?>" alt="<?=$image['caption']?>" class="rounded-circle" style="height:75px;">
+                                <?php endforeach;?>
+                            </div>
+                            <div class="d-flex flex-row align-items-center col-2">
                                 <div class="d-flex flex-column mr-2">
                                     <!-- put buttons in line -->
                                     <div class="btn_group edit-button">
-                                        <a href="./product-update.php?pid=<?=$product['id']?>" class="mx-1">
-                                            <button class="btn btn-info">
-                                                <i class="fas fa-wrench"></i>
-                                            </button>
+                                        <a href="./product-update.php?pid=<?=$product['id']?>" class="mx-1 btn btn-info">
+                                            <i class="fas fa-wrench"></i>
                                         </a>
                                         <button id="delete" class="btn btn-danger" value="<?=$product['id']?>" name="pid ">
                                             <i class="fa fa-minus"></i>
@@ -48,12 +67,15 @@
                                 </div>
                             </div>
                         </li>
-                    <?php endforeach; ?>
+                        <?php endforeach; ?>
                     </ul>
                 </div>
             </div>
         </div> 
+    </div>
 </div>
+
+<?=pagination($page, $total_pages, $current_page)?>
 
 <?php
     include_once './footer.php';
