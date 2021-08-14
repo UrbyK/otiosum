5380f49873965f7ec69f4cf1315db54cba1ff097
UrbyX9 <klinc.urban97@gmail.com>
2021-08-14 22:30:50 +0200

    Add check to see if any products are on sale or are rated so that id doesn't show enmpty fields

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
     <?php
         $query = "SELECT p.*, AVG(r.rating) AS ratings FROM product p INNER JOIN review r ON p.id = r.product_id GROUP BY p.id ORDER BY ratings DESC LIMIT 6";
         $bestRated = $pdo->query($query)->fetchAll(PDO::FETCH_ASSOC);
+        if($bestRated):
     ?>
     <div class="product-group card my-3">
         <h2 class="align-items-center mx-3 border-bottom">Najbolje ocenjeni</h2>
@@ -29,10 +30,12 @@
             } ?>
         </div>
     </div>
+    <?php endif; ?>
 
     <?php
         $query = "SELECT  DISTINCT p.* FROM product p INNER JOIN product_sale ps ON p.id = ps.product_id INNER JOIN sale s ON ps.sale_id = s.id WHERE s.date_start <= CURDATE() AND s.date_end >= CURDATE() ORDER BY s.date_start DESC LIMIT 6";
         $productsOnSale = $pdo->query($query)->fetchAll(PDO::FETCH_ASSOC);
+        if($productsOnSale):
     ?>
     <div class="product-group card my-3">
         <h2 class="align-items-center mx-3 border-bottom">Novo na razprodaji</h2>
@@ -42,6 +45,7 @@
             } ?>
         </div>
     </div>
+    <?php endif; ?>
 
 
 </div>
