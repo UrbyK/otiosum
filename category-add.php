14dc3ad821ac8fc1c9ae7744876558dc4e86fbf5
UrbyX9 <klinc.urban97@gmail.com>
2021-08-14 22:30:12 +0200

    Fix the alert classes

--- category-add.php
+++ category-add.php
@@ -15,13 +15,13 @@
         include_once './src/inc/error.inc.php';
         // check if given error exists in the error array 
         if (array_key_exists($_GET['error'], $errorList)): ?>
-            <div class="error w-100 mt-2 text-center alert-danger">
+            <div class="alert w-100 mt-2 text-center alert-danger rounded">
                 <h3><?=$errorList[$_GET['error']]?></h3>
             </div>
     
             <!-- if given error does not exist in the error array  -->
         <?php else: ?>
-            <div class="error w-100 mt-2 text-center alert-danger">
+            <div class="alert w-100 mt-2 text-center alert-danger rounded">
                 <h3>Zgodila se je neznana napaka. Prosim poskusite kasneje!</h3>
             </div>
         <?php endif; ?>
@@ -29,7 +29,7 @@
 
     // check if status is given in URL and if the status is success
     if (isset($_GET['status']) && isset($_GET['status']) == "success"):?>
-        <div class="error w-100 mt-2 text-center alert-success">
+        <div class="alert w-100 mt-2 text-center alert-success rounded">
             <h3>Kategorije uspešno vnesene!</h3>
         </div>
     <?php endif; ?>
