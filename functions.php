942497bae989dfcc1fdddb1a547d44003022db87
UrbyX9 <klinc.urban97@gmail.com>
2021-08-14 12:56:01 +0200

    Add comment, add function to show live update order-list for user

--- functions.php
+++ functions.php
@@ -1,10 +1,12 @@
 <?php
+    // db connection
     if(file_exists("./src/inc/dbh.inc.php")) {
         include './src/inc/dbh.inc.php';
     } else {
         include './dbh.inc.php';
     }
     
+    // pagination
     if(file_exists('./pagination.php')) {
         // pagination script
         include './pagination.php';
@@ -18,6 +20,7 @@
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $result;
     }
+    // all main categories
     function rootCategories(){
         $pdo = pdo_connect_mysql();
         $stmt = $pdo->prepare("SELECT * FROM category WHERE parent_id IS NULL");
@@ -295,5 +298,11 @@
         $pdo = pdo_connect_mysql();
         $query = "SELECT * FROM delivery_type";
         return $pdo->query($query)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function orderList($orders, $isPrivlage, $pdo) {
+        ob_start();
+            include './order-card.php';
+        return ob_get_clean();
     }
 ?>
