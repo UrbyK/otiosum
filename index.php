60360e83f1b98ef708b760c78e19df425ac49bf5
UrbyX9 <klinc.urban97@gmail.com>
2021-08-15 17:13:47 +0200

    Fix product showing by adding a forgoten where date_published <= curdate()

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     </div>
 
     <?php
-        $query = "SELECT p.*, AVG(r.rating) AS ratings FROM product p INNER JOIN review r ON p.id = r.product_id GROUP BY p.id ORDER BY ratings DESC LIMIT 6";
+        $query = "SELECT p.*, AVG(r.rating) AS ratings FROM product p INNER JOIN review r ON p.id = r.product_id WHERE date_published <= CURDATE() GROUP BY p.id ORDER BY ratings DESC LIMIT 6";
         $bestRated = $pdo->query($query)->fetchAll(PDO::FETCH_ASSOC);
         if($bestRated):
     ?>
@@ -33,7 +33,7 @@
     <?php endif; ?>
 
     <?php
-        $query = "SELECT  DISTINCT p.* FROM product p INNER JOIN product_sale ps ON p.id = ps.product_id INNER JOIN sale s ON ps.sale_id = s.id WHERE s.date_start <= CURDATE() AND s.date_end >= CURDATE() ORDER BY s.date_start DESC LIMIT 6";
+        $query = "SELECT  DISTINCT p.* FROM product p INNER JOIN product_sale ps ON p.id = ps.product_id INNER JOIN sale s ON ps.sale_id = s.id WHERE date_published <= CURDATE() AND s.date_start <= CURDATE() AND s.date_end >= CURDATE() ORDER BY s.date_start DESC LIMIT 6";
         $productsOnSale = $pdo->query($query)->fetchAll(PDO::FETCH_ASSOC);
         if($productsOnSale):
     ?>
